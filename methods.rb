71085310875c94c3ea7b8e4d8024021912e3a00a
Adam Romero <aromero0904@gmail.com>
2023-01-30 03:13:20 -0500

    commit tree ff26223472476295a223ca428a8334ed1811eef4

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default()
+
+def add (num1, num2)
+    sum = num1 + num2
+    return sum
+end
+
+def halve(num1)
+    if num1.class != Integer
+        return nil
+    end
+
+    num2 = num1 / 2
+    return num2
+end
+
+halve(6)
